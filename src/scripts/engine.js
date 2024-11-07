185ce846f807bff4fc1a0d1da14308d080520023
Kevyn Lima <kevynlimasilva1@gmail.com>
2024-11-07 19:43:15 -0300

    Implementando a logica de setar cartas em campo, ajustando o caminho dos recursos.

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,7 +18,13 @@ const state = {
     },
 };
 
-const pathImages = ".src/assets/icons/";
+const playerSides = {
+    player1:"player-cards",
+    computer:"computer-cards",
+}
+
+const pathImages = "./src/assets/icons/";
+
 const cardData = [
     {
         id:0,
@@ -46,7 +52,45 @@ const cardData = [
     },
 ];
 
-function init(){}
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random()* cardData.length)
+    return cardData[randomIndex].id
+}
+
+async function createCardImage(IdCard, fieldSide){
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", `${pathImages}card-back.png`);
+    cardImage.setAttribute("data-id", IdCard);
+    cardImage.classList.add("card");
+
+
+
+    if (fieldSide === playerSides.player1) {
+        cardImage.addEventListener('click', ()=>{
+            setCardsField(cardImage.getAttribute('data-id'))
+        })
+    }
+
+    cardImage.addEventListener("mouseover", () =>{
+        drawSelectCard(IdCard);
+    });
+    return cardImage;
+}
+
+async function drawCards(cardNumbers, fieldSide){
+    for(let i = 0; i < cardNumbers; i++){
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage)
+    }
+}
+
+function init(){
+    drawCards(5, playerSides.player1)
+    drawCards(5, playerSides.computer)
+}
 
 init();
 
